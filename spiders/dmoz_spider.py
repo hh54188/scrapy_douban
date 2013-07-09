a70d1ed5092f4c0ccf674d3dbb776c79b774bd4a
hh54188 <hh54188@yahoo.cn>
2013-07-09 18:07:52 +0800

    ready go home

--- spiders/dmoz_spider.py
+++ spiders/dmoz_spider.py
@@ -1,4 +1,18 @@
+# from scrapy.spider import BaseSpider
+
+# class DmozSpider(BaseSpider):
+#     name = "dmoz"
+#     allowed_domains = ["dmoz.org"]
+#     start_urls = [
+#         "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+#         "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+#     ]
+
+#     def parse(self, response):
+#         filename = response.url.split("/")[-2]
+#         open(filename, 'wb').write(response.body)
 from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
 
 class DmozSpider(BaseSpider):
     name = "dmoz"
@@ -9,5 +23,10 @@ class DmozSpider(BaseSpider):
     ]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2]
-        open(filename, 'wb').write(response.body)
+        hxs = HtmlXPathSelector(response)
+        sites = hxs.select('//ul/li')
+        for site in sites:
+            title = site.select('a/text()').extract()
+            link = site.select('a/@href').extract()
+            desc = site.select('text()').extract()
+            print title
