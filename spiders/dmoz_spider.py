9074222e689eef42b15381f7ba1645d0baa9a29e
invegas <juststayinvegas@gmail.com>
2013-07-09 22:17:44 +0800

    prototype complete

--- spiders/dmoz_spider.py
+++ spiders/dmoz_spider.py
@@ -1,32 +1,18 @@
-# from scrapy.spider import BaseSpider
-
-# class DmozSpider(BaseSpider):
-#     name = "dmoz"
-#     allowed_domains = ["dmoz.org"]
-#     start_urls = [
-#         "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-#         "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
-#     ]
-
-#     def parse(self, response):
-#         filename = response.url.split("/")[-2]
-#         open(filename, 'wb').write(response.body)
 from scrapy.spider import BaseSpider
 from scrapy.selector import HtmlXPathSelector
 
 class DmozSpider(BaseSpider):
-    name = "dmoz"
-    allowed_domains = ["dmoz.org"]
+    name = "douban"
+    allowed_domains = ["douban.com"]
     start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+        "http://www.douban.com/group/beijingzufang/discussion"
     ]
 
     def parse(self, response):
         hxs = HtmlXPathSelector(response)
-        sites = hxs.select('//ul/li')
-        for site in sites:
-            title = site.select('a/text()').extract()
-            link = site.select('a/@href').extract()
-            desc = site.select('text()').extract()
-            print title
+        rows = hxs.select("//table[@class='olt']/tr/td[@class='title']/a")
+        print "!!!!!!!!!!"
+        # print sites
+        for row in rows:
+            print row.select('text()').extract()[0]
+            print row.select('@href').extract()[0]
