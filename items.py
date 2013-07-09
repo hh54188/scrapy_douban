8cc9e90178d5dcde012786b43b26afcfcbe4a85b
hh54188 <hh54188@yahoo.cn>
2013-07-09 16:45:08 +0800

    move all tutorial here

---
+++ items.py
@@ -0,0 +1,11 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/topics/items.html
+
+# from scrapy.item import Item, Field
+
+# class DmozItem(Item):
+#     title = Field()
+#     link = Field()
+#     desc = Field()
