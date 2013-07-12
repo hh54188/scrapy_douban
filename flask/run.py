1f83dc3263fbac1929f2beec5f854ac6ade1ff30
invegas <juststayinvegas@gmail.com>
2013-07-13 01:12:20 +0800

    add template

--- flask/run.py
+++ flask/run.py
@@ -1,14 +1,22 @@
-from flask import Flask
+from flask import Flask, url_for
 from flask import request
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return 'Hello World!'
+	# return 'Hello World!'
+	# for i in range(10000):
+	# 	print i
+	return render_template('index.html')
 
-@app.route('/login', methods="POST")
-def login():
-	return 'login'
+@app.route('/login/<username>')
+def login(username):
+	return 'login %s' % (username)
+
+# @app.route('/login/<username>', methods="POST")
+# def login():
+# 	return 'login'
 
 app.debug = True
 app.run()
